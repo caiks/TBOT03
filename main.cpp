6c3d7832987b19a74be8d528da0c662476e1b341
Cliff McCartney <caiks@greenlake.co.uk>
2021-05-20 06:15:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,87 @@ typedef std::chrono::high_resolution_clock clk;
 
 int main(int argc, char **argv)
 {
+	if (argc >= 3 && string(argv[1]) == "view_records")
+	{
+		bool ok = true;
+		string model = string(argv[2]);
+	
+		EVAL(model);
+		
+		std::ifstream in(model + ".rec", std::ios::binary);
+		TRUTH(in.is_open());
+		ECHO(std::shared_ptr<TBOT03::RecordList> records = std::move(persistentsRecordList(in)));
+		EVAL(*records);
+	}
+	if (argc >= 3 && string(argv[1]) == "view_active")
+	{
+		bool ok = true;
+		string model = string(argv[2]);
+	
+		EVAL(model);
+		
+		std::unique_ptr<System> uu;
+		std::unique_ptr<SystemRepa> ur;
+		std::unique_ptr<HistoryRepa> hr;
+		if (ok) 
+		{
+			SystemHistoryRepaTuple xx = posesScansHistoryRepa(8, std::array<double,7>(), std::array<double,360>());	
+			uu = std::move(std::get<0>(xx));
+			ur = std::move(std::get<1>(xx));
+			hr = std::move(std::get<2>(xx));
+		}
 
+		Active activeA;
+		activeA.logging = true;		
+		if (ok) 
+		{
+			ActiveIOParameters ppio;
+			ppio.filename = model +".ac";
+			ok = ok && activeA.load(ppio);
+			TRUTH(ok);				
+		}		
+		if (ok)
+		{
+			EVAL(activeA.name);				
+			EVAL(activeA.underlyingEventUpdateds);		
+			std::size_t sizeA = activeA.historyOverflow ? activeA.historySize : activeA.historyEvent;				
+			EVAL(activeA.historySize);				
+			TRUTH(activeA.historyOverflow);				
+			EVAL(activeA.historyEvent);				
+			EVAL(sizeA);
+			EVAL(activeA.historyEvent);		
+			TRUTH(activeA.continousIs);				
+			EVAL(activeA.continousHistoryEventsEvent);				
+			for (auto& hr : activeA.underlyingHistoryRepa)
+			{
+				EVAL(hr->dimension);				
+				EVAL(hr->size);				
+				// EVAL(*hr);				
+			}
+			for (auto& hr : activeA.underlyingHistorySparse)
+			{
+				EVAL(hr->size);				
+				// EVAL(*hr);				
+			}			
+			EVAL(sorted(activeA.underlyingSlicesParent));				
+			EVAL(activeA.bits);				
+			EVAL(activeA.var);				
+			EVAL(activeA.varSlice);				
+			EVAL(activeA.induceThreshold);				
+			EVAL(activeA.induceVarExclusions);				
+			if (activeA.historySparse) {EVAL(activeA.historySparse->size);}
+			// if (activeA.historySparse) {EVAL(*activeA.historySparse);}				
+			EVAL(activeA.historySlicesSetEvent);				
+			EVAL(activeA.induceSlices);				
+			EVAL(activeA.induceSliceFailsSize);				
+			EVAL(activeA.frameUnderlyings);				
+			EVAL(activeA.frameHistorys);				
+			EVAL(activeA.framesVarsOffset);				
+			if (activeA.decomp) {EVAL(activeA.decomp->fuds.size());}
+			if (activeA.decomp) {EVAL(activeA.decomp->fudRepasSize);}
+			if (activeA.decomp) {EVAL((double)activeA.decomp->fuds.size() * activeA.induceThreshold / sizeA);}
+			// if (activeA.decomp) {EVAL(*activeA.decomp);}			
+		}
+	}
 	return 0;
 }
